e24ebb0dc13beafc74a9559d8a0ec5419a2a3f8b
nicoalejo <nicoalejor@gmail.com>
2023-05-02 08:57:59 -0500

    Changed window start.

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,18 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        mainForm formaPrincipal = new mainForm();
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                mainForm formaPrincipal = new mainForm();
 
-        formaPrincipal.setContentPane(formaPrincipal.getMainPanel());
-        formaPrincipal.setBounds(100,100,500,500);
-        formaPrincipal.setVisible(true);
-        formaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                formaPrincipal.setContentPane(formaPrincipal.getMainPanel());
+                formaPrincipal.setBounds(100,100,500,500);
+                formaPrincipal.setVisible(true);
+                formaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            }
+        });
+
+
 
     }
 }
